bd2a59d4edc582ed243c1b856e67b942d584bef6
Alpha <56209673+Alpxnz@users.noreply.github.com>
2020-09-07 18:30:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const fs = require('fs'),
-      ytdl = require("ytdl-core"),
-      readline = require('readline');
+const ytdl = require("ytdl-core")
+const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+      input: process.stdin,
+      output: process.stdout
 });
+
 
 rl.question('Digite uma URL do Youtube:', async (answer) => {
-  if(!ytdl.validateURL(answer)) return console.log('URL inválido.')
-  ytdl(answer).pipe(fs.createWriteStream('video.mp4'));
-  console.log('Vídeo baixado com sucesso.')
-  
-  rl.close();
-});
+      if(!ytdl.validateURL(answer)) return console.log('URL inválido.')
 
+      ytdl(answer).pipe(fs.createWriteStream('video.mp4'));
+      console.log('Vídeo baixado com sucesso.')
+      
+      rl.close();
+});
